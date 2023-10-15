3ef30a7847443b5c686f8a4a97426f926d787135
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-10-15 17:08:30 -0500

    primer intento de sensor de rotacion para girar drawable con sensor

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,7 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("com.github.MKergall:osmbonuspack:6.9.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- app/src/main/java/com/example/prueba/activities/HomeActivity.kt
+++ app/src/main/java/com/example/prueba/activities/HomeActivity.kt
@@ -4,18 +4,29 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
 import android.hardware.SensorManager
 import android.location.Address
 import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.prueba.R
 import com.example.prueba.databinding.ActivityHomeBinding
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.TileOverlay
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration
 import org.osmdroid.events.MapEventsReceiver
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
@@ -23,12 +34,17 @@ import org.osmdroid.util.GeoPoint
 import org.osmdroid.views.MapView
 import org.osmdroid.views.overlay.MapEventsOverlay
 import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Polyline
+import org.osmdroid.views.overlay.TilesOverlay
 
-class HomeActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityHomeBinding
     lateinit var map: MapView
     private lateinit var sensorManager: SensorManager
+    private lateinit var lightSensor: Sensor
+    private lateinit var linearAceleration: Sensor
+    private lateinit var orientationSensor: Sensor
     private lateinit var locationManager: LocationManager
     private val REQUEST_LOCATION_PERMISSION = 1
 
@@ -37,6 +53,13 @@ class HomeActivity : AppCompatActivity() {
     }
 
     private val userLocationMarkers = ArrayList<Marker>()
+
+    /*********************************************/
+    private val searchMarkers = ArrayList<Marker>()
+
+    private lateinit var roadManager: RoadManager
+    private var userGeoPoint: GeoPoint? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomeBinding.inflate(layoutInflater)
@@ -65,7 +88,11 @@ class HomeActivity : AppCompatActivity() {
         }
 
         sensorManager = getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        lightSensor = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT)
+        linearAceleration = sensorManager.getDefaultSensor(Sensor.TYPE_LINEAR_ACCELERATION)
+        orientationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR)
 
+      roadManager = OSRMRoadManager(this,"ANDROID")
 
     }
 
@@ -81,6 +108,10 @@ class HomeActivity : AppCompatActivity() {
         map.
         controller.animateTo(startPoint)
 
+        sensorManager.registerListener(this, lightSensor, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, linearAceleration, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, orientationSensor, SensorManager.SENSOR_DELAY_NORMAL)
+
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
 
             ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_LOCATION_PERMISSION)
@@ -95,13 +126,47 @@ class HomeActivity : AppCompatActivity() {
 
             }
 
+
+
             showUserLocation()
         }
+        /*
+        if (userGeoPoint == null) {
+            userGeoPoint = getUserLocation()
+        }
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+        } else {
+            showUserLocation()
+        } */
+
     }
     override fun onPause() {
         super.onPause()
         map.onPause()
     }
+
+    private fun getUserLocation(): GeoPoint? {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+            val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+            if (location != null) {
+                val userGeoPoint = GeoPoint(location.latitude, location.longitude)
+                map.controller.animateTo(userGeoPoint)
+                return userGeoPoint
+            } else {
+                // Handle the case where location is not available
+                return null
+            }
+        } else {
+            // Request location permission if not granted
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_LOCATION_PERMISSION)
+            return null
+        }
+    }
+
 
     private fun createOverlayEvents(): MapEventsOverlay{
 
@@ -113,6 +178,9 @@ class HomeActivity : AppCompatActivity() {
             override fun longPressHelper(p: GeoPoint?): Boolean {
                 if(p!= null){
                     longPressOnMap(p)
+
+                    Log.d("YourTag", "p: Latitude=${p?.latitude}, p=${p?.longitude}")
+
                 }
                 return true;
             }
@@ -123,6 +191,14 @@ class HomeActivity : AppCompatActivity() {
     private fun longPressOnMap(p: GeoPoint) {
 
         val address = findAddress(LatLng(p.latitude, p.longitude))
+        val snippet: String = address ?: ""
+
+        searchMarkers.forEach{map.overlays.remove(it)}
+        searchMarkers.clear()
+
+        val marker = createMarker(p, snippet, R.drawable.arrowofuser,"Unknown")
+        searchMarkers.add(marker)
+        map.overlays.add(marker)
 
     }
 
@@ -144,7 +220,7 @@ class HomeActivity : AppCompatActivity() {
             if(location != null){
 
                 val userGeoPoint = GeoPoint(location.latitude, location.longitude)
-                val userLocationMarker = createMarker(userGeoPoint, "Mi ubicacion", R.drawable.puntero2)
+                val userLocationMarker = createMarker(userGeoPoint, "Mi ubicacion", R.drawable.puntero2,"North")
                 userLocationMarkers.add(userLocationMarker)
                 map.overlays.add(userLocationMarker)
 
@@ -152,17 +228,175 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
-    private fun createMarker(p: GeoPoint, title: String, iconID: Int): Marker {
+    private fun createMarker(p: GeoPoint, title: String, iconID: Int, direccion: String): Marker {
 
         val marker = Marker(map)
         marker.title = title
         val myIcon = ContextCompat.getDrawable(this,  iconID)
         marker.icon = myIcon
         marker.position = p
+        val setAnchorByDirection: Marker.() -> Unit = {
+            when (direccion) {
+                "North" -> setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                "South" -> setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_TOP)
+                "East" -> setAnchor(Marker.ANCHOR_LEFT, Marker.ANCHOR_CENTER)
+                "West" -> setAnchor(Marker.ANCHOR_RIGHT, Marker.ANCHOR_CENTER)
+                "Northeast" -> setAnchor(Marker.ANCHOR_LEFT, Marker.ANCHOR_TOP)
+                "Southeast" -> setAnchor(Marker.ANCHOR_LEFT, Marker.ANCHOR_BOTTOM)
+                "Southwest" -> setAnchor(Marker.ANCHOR_RIGHT, Marker.ANCHOR_BOTTOM)
+                "Northwest" -> setAnchor(Marker.ANCHOR_RIGHT, Marker.ANCHOR_TOP)
+
+                else -> setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+            }
+
+        }
         marker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
         return marker
 
     }
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 
+    private val locationListener: LocationListener = object: LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val latitude = location.latitude
+            val longitude = location.longitude
+            val geoPoint = GeoPoint(latitude, longitude)
+
+            userLocationMarkers.forEach { map.overlays.remove(it) }
+            userLocationMarkers.clear()
+
+            val address = "Ubicacion Actual"
+            val userLocationMarker = createMarker(geoPoint, address, R.drawable.puntero2, "North")
+            userLocationMarkers.add(userLocationMarker)
+            map.overlays.add(userLocationMarker)
+        }
+
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) {
+
+        }
+    }
+        private fun searchLocation(address: String){
+
+            val addresses: List<Address> = geocoder.getFromLocationName(address, 1) as? List<Address> ?: emptyList()
+            if(addresses.isNotEmpty()){
+
+                val foundAddress = addresses[0]
+                val latitude = foundAddress.latitude
+                val longitude = foundAddress.longitude
+                val geoPoint = GeoPoint(latitude, longitude)
+
+                searchMarkers.forEach{map.overlays.remove(it)}
+                searchMarkers.clear()
+
+                val marker = createMarker(geoPoint, address, R.drawable.puntero2,"North")
+                searchMarkers.add(marker)
+                map.overlays.add(marker)
+
+                map.controller.animateTo(geoPoint)
+            }else{
+                Toast.makeText(baseContext, "Direccion no encontrada", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        private var roadOverlay: Polyline? = null
+
+        fun drawRoute(start: GeoPoint, finish: GeoPoint){
+            var routePoints = ArrayList<GeoPoint>()
+            routePoints.add(start)
+            routePoints.add(finish)
+            val road = roadManager.getRoad(routePoints)
+            Log.i("MapsApp", "Route length: "+road.mLength+" klm")
+            Log.i("MapsApp", "Duration: "+road.mDuration/60+" min")
+            if(map!=null){
+                if(roadOverlay != null){
+                    map.getOverlays().remove(roadOverlay);
+                }
+                roadOverlay = RoadManager.buildRoadOverlay(road)
+                roadOverlay!!.getOutlinePaint().setColor(
+                    Color.
+                RED)
+                roadOverlay!!.getOutlinePaint().setStrokeWidth(10F)
+                map.getOverlays().add(roadOverlay)
+            }
+
+        }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+      if(event?.sensor?.type == Sensor.TYPE_LIGHT){
+            val lightValue = event.values[0]
+            val threshold = 80.0
+            if(lightValue < threshold){
+                map.overlayManager.tilesOverlay.setColorFilter(TilesOverlay.INVERT_COLORS)
+            }else{
+                map.overlayManager.tilesOverlay.setColorFilter(null)
+            }
+        }
+
+        if(event?.sensor?.type == Sensor.TYPE_ROTATION_VECTOR) {
+
+            val rotationMatrix = FloatArray(9)
+            val orientationValues = FloatArray(3)
+
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values)
+            SensorManager.getOrientation(rotationMatrix, orientationValues)
+
+            val azimuthRadians = orientationValues[0]
+
+            val azimuthDegrees = Math.toDegrees(azimuthRadians.toDouble()).toFloat()
+
+            val adjustedAzimuth = if (azimuthDegrees < 0) azimuthDegrees + 360 else azimuthDegrees
+
+            val direction = mapHeadingToDirection(adjustedAzimuth)
+
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.ACCESS_FINE_LOCATION
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+
+                ActivityCompat.requestPermissions(
+                    this,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_LOCATION_PERMISSION
+                )
+            } else {
+                locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                val location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+
+                if (location != null) {
+                    val userGeoPoint = GeoPoint(location.latitude, location.longitude)
+                    createMarker(userGeoPoint,"user",R.drawable.arrowofuser,direction)
+                } else {
+
+                }
+            }
+        }
+        if(event?.sensor?.type == Sensor.TYPE_LINEAR_ACCELERATION){
+
+            val accelerationX = event.values[0]
+            val accelerationY = event.values[1]
+            val accelerationZ = event.values[2]
+
+
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {}
+
+    fun mapHeadingToDirection(heading: Float): String {
+        return when (heading) {
+            in 337.5..22.5, in 0.0..22.5 -> "North"
+            in 22.5..67.5 -> "Northeast"
+            in 67.5..112.5 -> "East"
+            in 112.5..157.5 -> "Southeast"
+            in 157.5..202.5 -> "South"
+            in 202.5..247.5 -> "Southwest"
+            in 247.5..292.5 -> "West"
+            in 292.5..337.5 -> "Northwest"
+            else -> "Unknown"
+        }
+    }
 }
+
+
